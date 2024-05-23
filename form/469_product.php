cbd4367af6c02087281a923261283e759547f803
Nqadhafi <qadhafi817@gmail.com>
2024-05-23 20:51:28 +0700

    editting product.php

--- form/469_product.php
+++ form/469_product.php
@@ -1,192 +1,193 @@
-<link rel="stylesheet" href="../css/custom.css">
-    <link rel="stylesheet" href="../css/bootstrap.min.css">
-<!-- <form method="POST"> -->
-  <form method="post">
-<div class="container-fluid bg-light mt-5 mx-auto p-3 w-75 rounded border border-warning">
+<link rel="stylesheet" href="./css/custom.css">
+<form method="POST">
+<div class="container-fluid p-3 w-75 rounded border border-warning">
 <div>
-  <h1 class="text-center">David Store</h1>
-  <h5>Masukkan detail order di bawah</h5>
+  <h5>2. Pilih produk yang mau kamu beli</h5>
   <hr >
 </div>
-  <div class="row">
-  <!-- Nama pelanggan -->
-  <div class="mb-3 col-md-6">
-    <label for="469_NamaPelanggan" class="form-label">Nama Pelanggan</label>
-    <input type="text" class="form-control" name="469_NamaPelanggan" required>
-  </div>
-  <!-- Nomor HP -->
-  <div class="mb-3 col-md-6">
-    <label for="469_NomorHP" class="form-label">Nomor HP</label>
-    <input type="text" class="form-control" name="469_NomorHP" required>
-  </div>
-  <!-- Email ID -->
-  <div class="mb-3 col-md-6">
-    <label for="469_Username" class="form-label">E-mail</label>
-    <input type="email" class="form-control" name="469_Email" required>
-  </div>
-  <!-- Password -->
-  <div class="mb-3 col-md-6">
-    <label for="469_Password" class="form-label">Password</label>
-    <input type="Password" class="form-control" name="469_Password" required>
-  </div>
-  </div>
-  <!-- Pilihan Rank Joki -->
-  <h6>Pilih Rank yang akan di joki :</h6>
+<!-- Radio Rank -->
+<h6>Pilih Rank yang akan di joki :</h6>
+<input type="hidden" name="form" value="form-prod">
 <div class=" row ms-5 mx-auto">
     <div class="form-check d-flex align-items-center col-md-6">
-      <input class="form-check-input me-3" type="radio" name="469_Rank" id="469_RankGrandMaster" value="Rank GrandMaster">
-      <label class="form-check-label" for="469_RankGrandMaster">
+      <input class="form-check-input me-3" type="radio" name="469_rank" id="469_rankGrandMaster" value="Rank GrandMaster">
+      <label class="form-check-label" for="469_rankGrandMaster">
       <h5>
-      <img src="../assets/grandmaster_icon.png" class="rank-icon_469">
+      <img src="./assets/grandmaster_icon.png" class="rank-icon_462">
       Rank GrandMaster</h5>   
       </label>
     </div>
     <div class="form-check d-flex align-items-center col-md-6">
-      <input class="form-check-input me-3" type="radio" name="469_Rank" id="469_RankEpic" value="Rank Epic">
-      <label class="form-check-label" for="469_RankEpic">
+      <input class="form-check-input me-3" type="radio" name="469_rank" id="469_rankEpic" value="Rank Epic">
+      <label class="form-check-label" for="469_rankEpic">
         <h5>
-      <img src="../assets/epic_icon.png" class="rank-icon_469">  
+      <img src="./assets/epic_icon.png" class="rank-icon_462">  
       Rank Epic 
       </h5>
       </label>
     </div>
     <div class="form-check d-flex align-items-center col-md-6">
-      <input class="form-check-input me-3" type="radio" name="469_Rank" id="469_RankLegend" value="Rank Legend">
-      <label class="form-check-label" for="469_RankLegend">
+      <input class="form-check-input me-3" type="radio" name="469_rank" id="469_rankLegend" value="Rank Legend">
+      <label class="form-check-label" for="469_rankLegend">
         <h5>
-      <img src="../assets/legend_icon.png" class="rank-icon_469">
+      <img src="./assets/legend_icon.png" class="rank-icon_462">
        Rank Legend 
       </h5>
       </label>
     </div>
     <div class="form-check d-flex align-items-center col-md-6">
-      <input class="form-check-input me-3" type="radio" name="469_Rank" id="469_RankMythic" value="Rank Mythic">
-      <label class="form-check-label" for="469_RankMythic">
+      <input class="form-check-input me-3" type="radio" name="469_rank" id="469_rankMythic" value="Rank Mythic">
+      <label class="form-check-label" for="469_rankMythic">
         <h5>
-      <img src="../assets/mythic_icon.png" class="rank-icon_469">  Rank Mythic 
+      <img src="./assets/mythic_icon.png" class="rank-icon_462">  Rank Mythic 
       </h5>
       </label>
     </div>
     </div>
-    <!-- Jumlah Bintang -->
+<!-- Jumlah Bintang -->
 <div class="form-group my-4">
-      <label for="469_Jumlah">
+      <label for="469_jumlahBintang">
         <h6>Jumlah Bintang:</h6>
       </label>
-      <input type="number" class="form-control" id="469_Jumlah" name="469_Jumlah" min="1" step="1" value="1">
+      <input type="number" class="form-control" id="469_jumlahBintang" name="469_jumlahBintang" min="1" step="1" value="1">
     </div>
-    <!-- Metode Pembayaran -->
+  <!-- Kecepatan Order -->
+  <h6>Kecepatan Order :</h6>
+  <div class="form-check">
+  <input class="form-check-input" type="checkbox" value="Turbo" id="469_KecepatanPaket" name="469_KecepatanPaket">
+  <label class="form-check-label" for="469_KecepatanPaket">
+    Paket Turbo (3-5 hari)
+  </label>
+</div>
+<div class="form-check">
+  <input class="form-check-input" type="checkbox" value="Reguler" id="469_KecepatanPaket" name="469_KecepatanPaket">
+  <label class="form-check-label" for="469_KecepatanPaket">
+    Paket Reguler (7-10 hari)
+  </label>
+</div>
+  <!-- Metode Pembayaran -->
   <h6 class="mt-4">Metode Pembayaran :</h6>
-  <select class="form-select" name="469_Metode" required>
+  <select class="form-select" name="469_MetodePembayaran">
   <option selected>Pilih Metode Pembayaran Anda</option>
   <option value="Dana">Dana</option>
   <option value="OVO">OVO</option>
+  <option value="Go-Pay">Go-Pay</option>
   <option value="Transfer">Bank Transfer</option>
+  <option value="COD">COD</option>
 </select>
-<!-- Button Submit -->
-  <div class="text-center mt-5 mb-3">
-  <button type="submit" class="btn btn-primary mx-2 px-5 py-3">Submit</button>
-  <a href="../">
-  <div class="btn btn-primary px-5 py-3">Kembali</div>
-  </a>
+<!-- Survey -->
+  <div class="mt-4 ">
+    <label for="Survey" class="form-label"><h6>Darimana kamu mendengar tentang kami?</h6></label>
+    <input type="text" class="form-control" name="469_Survey" id="Survey">
   </div>
-  <!-- Codingan PHP Objek -->
+  <div class="text-center mt-5 mb-3">
+  <button type="submit" class="btn btn-primary px-5 py-3">Order Sekarang</button>
+  </div>
+  <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST" ) {
+      //class product469 mempunyai 5 properti protected dan 1 construct method public
+      class Product469{
+        protected $rank469;
+        protected $jumlah469;
+        protected $kecepatan469;
+        protected $metode469;
+        protected $survey469;
 
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") { //Jika form mengirim request submit POST maka kode dibawah baru dijalanakn
-class dataOrder_469{ //deklarasi class customer
-  protected $nama469; //properti bersifat protected
-  protected $nohp469;
-  protected $email469;
-  protected $password469;
-  protected $rank469;
-  protected $jumlah469;
-  protected $metode469;
+        //memberi nilai kepada properti di atas melalui tangkapan data dari parameter
+        public function __construct($rank, $jumlah, $kecepatan, $metode,$survey){
+          $this->rank469 = $rank;
+          $this->jumlah469 = $jumlah;
+          $this->kecepatan469 = $kecepatan;
+          $this->metode469 = $metode;
+          $this->survey469 = $survey;
+        }
+      }
 
-  //method construct public untuk memberi nilai properti yang bersifat protected
-  public function __construct($nama,$nohp,$email,$password,$rank,$jumlah,$metode){ 
-      $this->nama469 = $nama;
-      $this->nohp469 = $nohp;
-      $this->email469 = $email;
-      $this->password469 = $password;
-      $this->rank469 = $rank;
-      $this->jumlah469 = $jumlah;
-      $this->metode469 = $metode;
-  }
-}
+      //Class proses469 mewarisi properti & construct dari class product469
+      class Proses469 extends Product469{
+        protected function setData469(){
+          //membuat variable baru nominalRank untuk set harga rank awal
+          $nominalRank = NULL;
+          //membuat variable baru paket untuk set harga kecepatan paket
+          $paket = NULL;
+          // memberi nilai pada nominalRank berdasar value inputan rank
+          switch($this->rank469){
+            case "Rank GrandMaster":
+              $nominalRank = 5000;
+              break;
+          case "Rank Epic":
+            $nominalRank = 10000;
+              break;
+          case "Rank Legend":
+            $nominalRank = 15000;
+              break;
+          case "Rank Mythic":
+            $nominalRank = 20000;
+              break;
+          }
+          // memberi nilai harga pada paket berdasarkan value inputan
+          if($this->kecepatan469 == "Turbo"){
+            $paket = 15000;
+          }
+          else{
+            $paket = 0;
+          }
+          //rumus memberi total harga joki
+          $totalHarga = $nominalRank * $this->jumlah469 + $paket;
 
-class Proses_469 extends dataOrder_469{ //class proses mewarisi properti & method class customer_469
-  
-  //method protected untuk menampilkan setting tampilan inputan
-  protected function setData469(){
-
-    //membuat variable baru nominalRank untuk set harga rank awal
-    $nominalRank = NULL;
-
-      // memberi nilai pada nominalRank berdasar value inputan rank
-    switch($this->rank469){
-      case "Rank GrandMaster":
-        $nominalRank = 5000;
-        break;
-    case "Rank Epic":
-      $nominalRank = 10000;
-        break;
-    case "Rank Legend":
-      $nominalRank = 15000;
-        break;
-    case "Rank Mythic":
-      $nominalRank = 20000;
-        break;
-    }
-    //rumus memberi total harga joki
-    $totalHarga469 = $nominalRank * $this->jumlah469;
       echo "<div class='container-fluid p-3 w-75 rounded border border-success mt-4'>";
       echo "<h5>Data Order Anda:</h5><hr>";
       echo "<p><strong>Paket Joki $this->rank469 - $this->jumlah469 Bintang</strong> </p>";
-      echo "<p><strong>Nama Pelanggan:</strong> $this->nama469</p>";
-      echo "<p><strong>Nomor HP:</strong> $this->nohp469</p>";
-      echo "<p><strong>E-mail:</strong> $this->email469</p>";
-      echo "<p><strong>Password:</strong> $this->password469</p>";
+      echo "<p><strong>Tipe Paket:</strong> $this->kecepatan469</p>";
       echo "<p><strong>Metode Pembayaran:</strong> $this->metode469</p>";
-      echo "<p><strong>Total Bayar:</strong> $totalHarga469</p>";
+      echo "<p><strong>Referal :</strong> $this->survey469</p>";
+      echo "<p><strong>Total Bayar:</strong> $totalHarga</p>";
       echo "</div>";
-    }
-//method public untuk mengambil data dari method setData yang bersifat protected
-public function displayData469() {
-  // Deklarasi variable email yang digunakan, isinya array
-  $emaildigunakan = ["abcd@gmail.com", "david@gmail.com", "robby123@gmail.com"];
-      if (in_array($this->email469, $emaildigunakan)) {
-          // Jika data di dalam inputan email ada yang sama dengan yang di dalam array $emaildigunakan  maka eksekusi kode dibawah
-          echo "<div class='container-fluid p-3 w-75 rounded border border-danger mt-4'>";
-          echo "<h5>Email '$this->email469' sudah digunakan.</h5>";
-          echo "</div>";
+        }
+
+      public function displayData469(){
+        //deklarasi variable isi array metode pembayaran diblokir
+        $metodeBlokir = ["DANA","OVO"];
+        //deklarasi array inputan
+        $arrayInputProd = [
+          $this->rank469,
+          $this->jumlah469,
+          $this->kecepatan469,
+          $this->metode469,
+          $this->survey469,
+      ];
+      foreach ($arrayInputProd as $dataProd) {
+        if (in_array($this->metode469, $metodeBlokir)) {
+            // Jika data di dalam inputan metode ada yang sama dengan yang di dalam array $metodeBlokir atau, maka eksekusi kode dibawah
+            echo "<div class='container-fluid p-3 w-75 rounded border border-danger mt-4'>";
+            echo "<h5>Pembayaran menggunakan '$this->metode469' sedang tidak bisa digunakan.</h5>";
+            echo "</div>";
+            return;
+        }
+        //jika inputan data kosong, tidak menampilkan apa-apa
+        else if (empty($dataProd)){
+          echo "";
           return;
+        }
+    }
+    // Jika tidak ada yang sama dan terisi semua, maka tampilkan data yang diambil dari setData
+    $this->setData469();
       }
-  
-  // Jika tidak ada yang sama dan terisi semua, maka tampilkan data yang diambil dari setData
-  $this->setData469();
-}
 
+      }
+      //membuat objek dengan parameter tangkapan dari submit POST
+      $berinilai469 = new Proses469(
+          $_POST['469_rank']?? '',
+          $_POST['469_jumlahBintang'] ?? '',
+          $_POST['469_KecepatanPaket'] ?? '',
+          $_POST['469_MetodePembayaran'] ?? '',
+          $_POST['469_Survey'] ?? ''
+      );
+
+      //mengeksekusi method displaydata469
+      echo $berinilai469->displayData469();
     }
 
-//membuat objek dengan parameter tangkapan data dari POST
-$berinilai469 = new Proses_469(
-                $_POST['469_NamaPelanggan'],
-                $_POST['469_NomorHP'],
-                $_POST['469_Email'],
-                $_POST['469_Password'],
-                $_POST['469_Rank'],
-                $_POST['469_Jumlah'],
-                $_POST['469_Metode']
-              );
-
-//mengeksekusi logika yang ada di method displayData
-echo $berinilai469->displayData469();
-}?>
-</div>
-
+  ?>
+  </div>
 </form>
-
-<script src="./css/bootstrap.bundle.min.js"></script>
-<!-- </form> -->
-
