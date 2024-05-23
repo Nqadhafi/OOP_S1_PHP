e9c9163053290dd1488ce71d38f23e97806a14d2
Nqadhafi <qadhafi817@gmail.com>
2024-05-23 20:53:48 +0700

    final commit revisi (killmeplz)

--- index.php
+++ index.php
@@ -14,23 +14,10 @@
     <h1>Jasa Joki Mobile Legends Bang Bang</h1>
   </div>
   <!-- Form dengan method post untuk menigirim data -->
-  
     <!-- Memanggil form dengan fungsi include supaya codingan lebih rapi -->
-    <div class="text-center mt-5 mb-3">
-      <h5>Klik Di Sini Untuk Masuk Ke Haikal Store</h5>
-      <a href="./form/462_customer.php">
-  <button class="btn btn-primary px-5 py-3">Click Me!</button>
-  </a>
-  </div>
+  <?php include ('./form/462_customer.php'); ?>
   <br>
-  <div class="text-center mt-5 mb-3">
-  <h5>Klik Di Sini Untuk Masuk Ke David Store</h5>
-  <a href="./form/469_product.php">
-  <button class="btn btn-primary px-5 py-3">Click Me!</button>
-  </a>
-  </div>
-  </div>
-  
+  <?php include ('./form/469_product.php'); ?>
 </div>
 </body>
 <script src="./css/bootstrap.bundle.min.js"></script>
