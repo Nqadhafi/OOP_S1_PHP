5ac3907b2d5d73f8c7903c573751aa6b7893235c
Nqadhafi <qadhafi817@gmail.com>
2024-06-02 13:36:55 +0700

    changing title

--- login.php
+++ login.php
@@ -55,7 +55,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login Pelanggan</title>
     <link rel="stylesheet" href="./css/custom.css">
     <link rel="stylesheet" href="./css/bootstrap.min.css">
 </head>
